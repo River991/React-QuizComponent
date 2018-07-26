8a4c9f5424860a26047f863da646256d6a6e47ff
River Phillips <river.phillips@asos.com>
2018-07-26 16:10:46 +0100

    Added reset functionality

--- src/Quiz.js
+++ src/Quiz.js
@@ -18,20 +18,34 @@ class Quiz extends Component {
     });
   }
 
+  handleResetClick() {
+    this.setState(() => {
+      return { quiz_position: 1 };
+    });
+  }
+
   render() {
     const isQuizEnd =
       this.state.quiz_position - 1 === quizData.quiz_questions.length;
 
-    let display = isQuizEnd ? (
-      <QuizEnd />
-    ) : (
-      <QuizQuestion
-        showNextQuestionHandler={this.showNextQuestion.bind(this)}
-        quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
-      />
+    return (
+      <div>
+        {isQuizEnd ? (
+          <QuizEnd
+            className="QuizEnd"
+            resetClickHandler={this.handleResetClick.bind(this)}
+          />
+        ) : (
+          <QuizQuestion
+            className="QuizQuestion"
+            showNextQuestionHandler={this.showNextQuestion.bind(this)}
+            quiz_question={
+              quizData.quiz_questions[this.state.quiz_position - 1]
+            }
+          />
+        )}
+      </div>
     );
-
-    return <div>{display}</div>;
   }
 }
 
